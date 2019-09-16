bfab9b0eb62389bdc299766ac2015b505272dbea
joekennerly <joseph.kennerly@gmail.com>
2019-09-16 11:23:29 -0500

    data tables are good to go

--- student-exercise-3.sql
+++ student-exercise-3.sql
@@ -4,9 +4,11 @@ CREATE TABLE Cohort (
 );
 
 CREATE TABLE Student_Exercise (
-	StuExId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	FOREIGN KEY(StudentId) REFERENCES Student(StudentId),
-	FOREIGN KEY(ExerciseId) REFERENCES Exercise(ExerciseId)
+	Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	StudentId INTEGER NOT NULL,
+    ExerciseId INTEGER NOT NULL,
+    FOREIGN KEY(StudentId) REFERENCES Student(StudentId),
+    FOREIGN KEY(ExerciseId) REFERENCES Exercise(ExerciseId)
 );
 
 CREATE TABLE Exercise (
@@ -20,8 +22,12 @@ CREATE TABLE Student (
     FirstName TEXT NOT NULL UNIQUE,
     LastName TEXT NOT NULL UNIQUE,
     SlackHandle TEXT NOT NULL UNIQUE,
+    CohortId INTEGER NOT NULL,
     FOREIGN KEY(CohortId) REFERENCES Cohort(CohortId)
 );
+
+ALTER TABLE Student 
+ADD CohortId INT FOREIGN KEY;
 
 CREATE TABLE Instructor (
     InstId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -29,6 +35,7 @@ CREATE TABLE Instructor (
     LastName TEXT NOT NULL UNIQUE,
     SlackHandle TEXT NOT NULL UNIQUE,
     Speciality TEXT NOT NULL UNIQUE,
+    CohortId INTEGER NOT NULL,
     FOREIGN KEY(CohortId) REFERENCES Cohort(CohortId)
 );
 
@@ -50,11 +57,33 @@ VALUES
 	(null, "Chicken Money", "Python");
 
 -- Add 3 Instructors
-INSERT INTO Instructor (InstId, Name)
+INSERT INTO Instructor (InstId, FirstName, LastName, SlackHandle, Speciality, CohortId)
 VALUES 
-	(null, "33")
-	(null, "34"),
-	(null, "35");
+	(null, "Joe", "Shep", "JoeS", "Dad Jokes", 1),
+	(null, "Steve", "Brownlee", "Coach", "Everything",2),
+	(null, "Leah", "Hoefling", "Leah", "SQL", 3);
+
+DROP TABLE Student_Exercise;
+
+-- Add 7 Students
+INSERT INTO Student (StudentId, FirstName, LastName, SlackHandle, CohortId)
+VALUES 
+	(null, "eoj", "d", "orgkenenn", 1),
+	(null, "Bab", "Ba", "aj", 2),
+	(null, "James", "Ga", "jahk", 3);
+
+INSERT INTO Student_Exercise (Id, StudentId, ExerciseId)
+VALUES
+	(null, 1, 1),
+	(null, 1, 2),
+	(null, 2, 1),
+	(null, 2, 3),
+	(null, 3, 1),
+	(null, 3, 4),
+	(null, 4, 1),
+	(null, 4, 5),
+	(null, 5, 1),
+	(null, 5, 6);
 
 SELECT * FROM Student;
 SELECT * FROM Instructor;
